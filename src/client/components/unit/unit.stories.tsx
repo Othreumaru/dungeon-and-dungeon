73b2aeeab14c9a099ac1f16a02dfdc3db3f3dd58
Krzysztof Kabat <odrinwhite@gmail.com>
2024-10-19 23:20:48 +0100

    fix: storybook

--- src/client/components/unit/unit.stories.tsx
+++ src/client/components/unit/unit.stories.tsx
@@ -1,6 +1,7 @@
 import { DemoScene } from "../demo-scene/demo-scene";
 import { Unit } from "./unit";
 import { StoryFn } from "@storybook/react";
+import { Unit as UnitType } from "../../../api";
 
 export default {
   title: "molecule/Unit",
@@ -24,14 +25,11 @@ export default {
       min: 0,
       max: 100,
     },
-  },
-};
-
-const Template = ({ now }: { now: number }) => (
-  <>
-    <Unit
-      now={now}
-      unit={{
+    unit: {
+      control: {
+        type: "object",
+      },
+      defaultValue: {
         id: "1",
         type: "moving",
         path: [
@@ -62,9 +60,50 @@ const Template = ({ now }: { now: number }) => (
           },
         ],
         color: "blue",
-      }}
-    />
+      },
+    },
+  },
+};
+
+const Template: StoryFn<{ now: number; unit: UnitType }> = ({ now, unit }) => (
+  <>
+    <Unit now={now} unit={unit} />
   </>
 );
 
 export const Sample1 = Template.bind({});
+Sample1.args = {
+  now: 1,
+  unit: {
+    id: "1",
+    type: "moving",
+    path: [
+      {
+        x: 0,
+        y: 0,
+        frame: 100,
+      },
+      {
+        x: 1,
+        y: 0,
+        frame: 200,
+      },
+      {
+        x: 1,
+        y: 1,
+        frame: 300,
+      },
+      {
+        x: 0,
+        y: 1,
+        frame: 400,
+      },
+      {
+        x: 0,
+        y: 0,
+        frame: 500,
+      },
+    ],
+    color: "blue",
+  },
+};
