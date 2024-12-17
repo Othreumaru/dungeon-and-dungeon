967cc05c20da23f0a3dee8ee4aefc88797feac56
Krzysztof Kabat <odrinwhite@gmail.com>
2024-12-17 21:42:04 +0000

    chore: frame tick refactor

--- src/engine/reducers/frame-tick-reducer.ts
+++ src/engine/reducers/frame-tick-reducer.ts
@@ -1,64 +1,65 @@
 import type { Actions } from "../../protocol/actions.ts";
 import type { State, Unit } from "../../protocol/state.ts";
 import {
-  isAnyUnitDoneMoving,
   isUnitActionCooldownReady,
   isUnitAnyActionCooldownReady,
   isUnitDoneMoving,
 } from "../selectors.ts";
+import { getUnchangedArray, getUnchangedObject } from "./compare-and-return.ts";
 
 export const frameTickReducer = (
   state: State,
   action: Extract<Actions, { type: "action:frame-tick" }>
 ): State => {
-  const doneMoving = isAnyUnitDoneMoving(state.units, action.payload.frame);
-  const actionCooldownReady = state.units.some((unit) =>
-    isUnitAnyActionCooldownReady(unit, action.payload.frame)
-  );
-  return doneMoving || actionCooldownReady
-    ? {
-        ...state,
-        units: state.units
-          .map(
-            (unit): Unit =>
-              isUnitDoneMoving(unit, action.payload.frame)
-                ? {
-                    ...unit,
-                    state: {
-                      type: "stationary",
-                      position: {
-                        x: unit.state.path[unit.state.path.length - 1].x,
-                        y: unit.state.path[unit.state.path.length - 1].y,
-                      },
-                      lookAt: {
-                        x: unit.state.path[unit.state.path.length - 1].x + 1,
-                        y: unit.state.path[unit.state.path.length - 1].y,
-                      },
-                    },
-                  }
-                : unit
-          )
-          .map((unit) => {
-            if (isUnitAnyActionCooldownReady(unit, action.payload.frame)) {
-              return {
-                ...unit,
-                actions: unit.actions.map((unitAction) => {
-                  if (
-                    isUnitActionCooldownReady(unitAction, action.payload.frame)
-                  ) {
-                    return {
-                      ...unitAction,
-                      state: {
-                        type: "ready",
-                      },
-                    };
-                  }
-                  return unitAction;
-                }),
-              };
-            }
-            return unit;
-          }),
+  return getUnchangedObject(state, {
+    ...state,
+    units: getUnchangedArray(
+      state.units,
+      state.units
+        .map((unit) => movingToStationary(unit, action.payload.frame))
+        .map((unit) => cooldownReady(unit, action.payload.frame))
+    ),
+  });
+};
+
+const movingToStationary = (unit: Unit, frame: number): Unit => {
+  if (!isUnitDoneMoving(unit, frame)) {
+    return unit;
+  }
+  const path = unit.state.path;
+  const lastPath = path[path.length - 1];
+  return {
+    ...unit,
+    state: {
+      type: "stationary",
+      position: {
+        x: lastPath.x,
+        y: lastPath.y,
+      },
+      lookAt: {
+        x: lastPath.x + 1,
+        y: lastPath.y,
+      },
+    },
+  };
+};
+
+const cooldownReady = (unit: Unit, frame: number): Unit => {
+  if (!isUnitAnyActionCooldownReady(unit, frame)) {
+    return unit;
+  }
+  return {
+    ...unit,
+    actions: unit.actions.map((unitAction) => {
+      if (isUnitActionCooldownReady(unitAction, frame)) {
+        return {
+          ...unitAction,
+          state: {
+            type: "ready",
+          },
+        };
       }
-    : state;
+      return unitAction;
+    }),
+  };
 };
