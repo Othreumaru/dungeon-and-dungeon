733242479441c8442c9452b5a4c49ee343415322
Krzysztof Kabat <odrinwhite@gmail.com>
2024-12-27 22:49:57 +0000

    feat: chat window 2.0 (#165)
    * feat: chat window 2.0
    
    * fix: window movement
    
    * fix: window resize
    
    * fix: opacity

--- src/client/components/chat-window/chat-window.tsx
+++ src/client/components/chat-window/chat-window.tsx
@@ -1,11 +1,69 @@
-import { useContext, useEffect, useRef, useState } from "react";
-import "./chat-window.css";
-import { useServerContext } from "../../hooks/use-server-context";
-import { Actions } from "../../../protocol/actions";
+// import { useContext, useEffect, useRef, useState } from "react";
+// import "./chat-window.css";
 import { EngineContext } from "../../engine-context";
 import { emitChatRequest } from "../../../protocol/requests";
+import {
+  rem,
+  Tabs,
+  Group,
+  Paper,
+  Box,
+  ScrollArea,
+  Flex,
+  Input,
+  Text,
+} from "@mantine/core";
+import {
+  IconPhoto,
+  IconMessageCircle,
+  IconSettings,
+  IconGridDots,
+  IconBaselineDensityMedium,
+} from "@tabler/icons-react";
+import { useDrag } from "../../hooks/use-drag";
+import { useElementSize, useLocalStorage } from "@mantine/hooks";
+import { useContext, useEffect, useRef, useState } from "react";
+import { Actions } from "../../../protocol/actions";
+import { useServerContext } from "../../hooks/use-server-context";
 
 export function ChatWindow() {
+  const { ref: resizeRef } = useElementSize();
+  const [windowHeight] = useState(300);
+  const [selectedTab, setSelectedTab] = useLocalStorage<string | null>({
+    key: "window-tab",
+    defaultValue: "chat",
+  });
+  const [windowSize, setWindowSize] = useLocalStorage({
+    key: "window-size",
+    defaultValue: {
+      width: 400,
+      height: 300,
+    },
+  });
+  const [windowPosition, setWindowPosition] = useLocalStorage({
+    key: "window-position",
+    defaultValue: {
+      x: 0,
+      y: (window.innerHeight - windowHeight) / window.innerHeight,
+    },
+  });
+  const { ref: dragRef } = useDrag({
+    onDrag: (e) => {
+      setWindowSize({
+        width: Math.max(e.pageX - windowPosition.x, 400),
+        height: Math.max(e.pageY - windowPosition.y, 300),
+      });
+    },
+  });
+  const { ref } = useDrag({
+    onDrag: (e) => {
+      setWindowPosition({
+        x: e.pageX,
+        y: e.pageY,
+      });
+    },
+  });
+
   const eventEmitter = useServerContext();
   const state = useContext(EngineContext);
   const [actions, setActions] = useState<Actions[]>([]);
@@ -26,51 +84,143 @@ export function ChatWindow() {
     };
   }, [eventEmitter]);
 
+  const iconStyle = { width: rem(12), height: rem(12) };
+
   return (
-    <>
-      <div className="chat-window">
-        <div className="chat-window-header">Chat Window</div>
-        <div className="chat-window-content">
-          <div className="chat-window-chat-messages" ref={messagesRef}>
-            {actions.map((message, index) =>
-              message.type === "action:chat" ? (
-                <div
-                  key={index}
-                  className="chat-window-chat-message"
-                  style={{
-                    color:
-                      state.units.find(
-                        (unit) => unit.id === message.payload.userId
-                      )?.color ?? "black",
-                  }}
-                >
-                  {`${
-                    state.units.find(
-                      (unit) => unit.id === message.payload.userId
-                    )?.name ?? "Unknown"
-                  }: ${message.payload.message}`}
-                </div>
-              ) : (
-                <div key={index} className="chat-window-chat-action">
-                  {JSON.stringify(message)}
-                </div>
-              )
-            )}
-          </div>
-        </div>
-        <div className="chat-window-chat-input-container">
-          <input
-            type="text"
-            placeholder="Type a message..."
-            onKeyUp={(event) => {
-              if (event.key === "Enter") {
-                emitChatRequest(eventEmitter, event.currentTarget.value);
-                event.currentTarget.value = "";
-              }
+    <div
+      style={{
+        position: "fixed",
+        top: `${windowPosition.y}px`,
+        left: `${windowPosition.x}px`,
+      }}
+    >
+      <Paper
+        withBorder
+        shadow="sm"
+        radius="md"
+        style={{ width: rem(windowSize.width), height: rem(windowSize.height) }}
+        ref={resizeRef}
+        opacity={0.7}
+      >
+        <Tabs
+          value={selectedTab}
+          onChange={setSelectedTab}
+          variant="outline"
+          h={"calc(100% - 40px)"}
+        >
+          <Group justify="start">
+            <Box component="div" ref={ref} p={rem(8)}>
+              <IconGridDots
+                style={{ width: rem(24), height: rem(24), opacity: 0.3 }}
+              />
+            </Box>
+            <Tabs.List>
+              <Tabs.Tab
+                value="chat"
+                leftSection={<IconPhoto style={iconStyle} />}
+              >
+                Chat
+              </Tabs.Tab>
+              <Tabs.Tab
+                value="messages"
+                leftSection={<IconMessageCircle style={iconStyle} />}
+              >
+                Messages
+              </Tabs.Tab>
+              <Tabs.Tab
+                value="state-debug"
+                leftSection={<IconSettings style={iconStyle} />}
+              >
+                State Debug
+              </Tabs.Tab>
+            </Tabs.List>
+          </Group>
+          <Tabs.Panel value="chat" h={"100%"}>
+            <Flex
+              direction="column"
+              p={10}
+              justify={"space-between"}
+              h={"100%"}
+            >
+              <ScrollArea w={"100%"} h={"100%"}>
+                <Flex direction="column" p={10}>
+                  {actions
+                    .filter((action) => action.type === "action:chat")
+                    .map((message, index) => (
+                      <Text
+                        key={index}
+                        fz="xs"
+                        lh="xs"
+                        ta="left"
+                        style={{
+                          color:
+                            state.units.find(
+                              (unit) => unit.id === message.payload.userId
+                            )?.color ?? "black",
+                        }}
+                      >
+                        {`${
+                          state.units.find(
+                            (unit) => unit.id === message.payload.userId
+                          )?.name ?? "Unknown"
+                        }: ${message.payload.message}`}
+                      </Text>
+                    ))}
+                </Flex>
+              </ScrollArea>
+              <Input
+                placeholder="Send meessage..."
+                onKeyUp={(event) => {
+                  if (event.key === "Enter") {
+                    emitChatRequest(eventEmitter, event.currentTarget.value);
+                    event.currentTarget.value = "";
+                  }
+                }}
+                mb={rem(5)}
+              />
+            </Flex>
+          </Tabs.Panel>
+          <Tabs.Panel value="messages" h={"100%"}>
+            <ScrollArea w={"100%"} h={"100%"} scrollbars="y" p={10}>
+              <Flex direction="column" gap={rem(10)} w={windowSize.width}>
+                {actions.map((message, index) => (
+                  <Text fz="xs" lh="xs" ta="left" m={rem(5)} key={index}>
+                    {JSON.stringify(message)}
+                  </Text>
+                ))}
+              </Flex>
+            </ScrollArea>
+          </Tabs.Panel>
+          <Tabs.Panel value="state-debug" h={"100%"}>
+            <ScrollArea w={"100%"} h={"100%"} scrollbars="y" p={10}>
+              <Text ta="left" fz="xs" lh="xs" component="pre">
+                {JSON.stringify(state, null, 2)}
+              </Text>
+            </ScrollArea>
+          </Tabs.Panel>
+        </Tabs>
+        <div
+          style={{
+            position: "absolute",
+            bottom: 0,
+            right: 0,
+            width: "16px",
+            height: "16px",
+            opacity: 0.3,
+            overflow: "hidden",
+            userSelect: "none",
+          }}
+          ref={dragRef}
+        >
+          <IconBaselineDensityMedium
+            style={{
+              width: rem(24),
+              height: rem(24),
+              transform: "rotate(-45deg) translate(0, 4px)",
             }}
           />
         </div>
-      </div>
-    </>
+      </Paper>
+    </div>
   );
 }
--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app.tsx";
-import "./index.css";
+import "@mantine/core/styles.css";
+// import "./index.css";
 import { SessionProvider } from "./session-context.tsx";
+import { MantineProvider } from "@mantine/core";
+import { theme } from "./theme";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <SessionProvider basePath="/auth">
-      <App />
+      <MantineProvider theme={theme}>
+        <App />
+      </MantineProvider>
     </SessionProvider>
   </StrictMode>
 );
